223179f47286bc61b80714a944d8bb78e286ac99
Hannah Park <teyang610@gmail.com>
2018-06-12 17:10:51 -0700

    structure html css

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,4 +1,7 @@
-function inser(num) {
+function insert(num) {
   Document.form.textview.value = document.form.textview.value + num;
+}
+function equal() {
+  document.form.textview.value = eval(document.form.textview.value);
 }
 //# sourceMappingURL=main.js.map
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,9 @@
-function inser(num) {
+function insert(num) {
 Document.form.textview.value=document.form.textview.value+num
+}
+function equal() {
+  val exp = document.form.textview.value;
+  if(exp) {
+  document.form.textview.value=eval(exp)
+  }
 }
